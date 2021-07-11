3e0fb156d801a885dec848d051e2cdfea998636b
Mikkel Buhl <mikkelnbuhl@gmail.com>
2021-07-11 05:47:28 +0200

    Added docker compose file

--- Play.Catalog/Play.Catalog.Api/Startup.cs
+++ Play.Catalog/Play.Catalog.Api/Startup.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
@@ -12,13 +11,6 @@ namespace Play.Catalog.Api
 {
     public class Startup
     {
-        private readonly IConfiguration _configuration;
-
-        public Startup(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
