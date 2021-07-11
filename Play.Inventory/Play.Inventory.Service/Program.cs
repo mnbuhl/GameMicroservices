bb70929d287c62e7a35c64d136cefae683a6f3c3
Mikkel Buhl <mikkelnbuhl@gmail.com>
2021-07-11 06:17:09 +0200

    Refactored project names and created Play.Inventory

---
+++ Play.Inventory/Play.Inventory.Service/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+// ReSharper disable All
+
+namespace Play.Inventory.Service
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
