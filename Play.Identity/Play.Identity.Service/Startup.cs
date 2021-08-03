bc650f1b4637bf5262c83d71d1ff71d2911323b9
Mikkel Buhl <mikkelnbuhl@gmail.com>
2021-08-03 22:43:19 +0200

    Added ApiResources collection to Identity Server Settings

--- Play.Identity/Play.Identity.Service/Startup.cs
+++ Play.Identity/Play.Identity.Service/Startup.cs
@@ -44,10 +44,11 @@ namespace Play.Identity.Service
                 })
                 .AddAspNetIdentity<AppUser>()
                 .AddInMemoryApiScopes(identityServerSettings.ApiScopes)
+                .AddInMemoryApiResources(identityServerSettings.ApiResources)
                 .AddInMemoryClients(identityServerSettings.Clients)
                 .AddInMemoryIdentityResources(identityServerSettings.IdentityResources)
                 .AddDeveloperSigningCredential();
-            
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
